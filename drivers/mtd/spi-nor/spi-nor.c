db39f5ed01f34cda26e87ee8378e35d55ea511c5
Anders Lindvall <anders.lindvall@embeddedartists.com>
2018-03-09 15:20:43 +0100

    Added support for DDR QUAD read for is25lp064a

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -576,7 +576,7 @@ static const struct spi_device_id spi_nor_ids[] = {
 
 	/* ISSI */
 	{ "is25cd512", INFO(0x7f9d20, 0, 32 * 1024,   2, SECT_4K) },
-	{ "is25lp064a", INFO(0x9d6017, 0, 64 * 1024,   128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "is25lp064a", INFO(0x9d6017, 0, 64 * 1024,   128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_DDR_QUAD_READ) },
 
 	/* Macronix */
 	{ "mx25l2005a",  INFO(0xc22012, 0, 64 * 1024,   4, SECT_4K) },
@@ -983,6 +983,7 @@ static int set_ddr_quad_mode(struct spi_nor *nor, struct flash_info *info)
 		}
 		return status;
 	case CFI_MFR_MACRONIX:
+	case CFI_MFR_PMC: /* The ISSI/PMC is25lp064a use the same register/setting */
 		status = macronix_quad_enable(nor);
 		if (status) {
 			dev_err(nor->dev,
@@ -1004,6 +1005,7 @@ static int set_quad_mode(struct spi_nor *nor, struct flash_info *info)
 
 	switch (JEDEC_MFR(info)) {
 	case CFI_MFR_MACRONIX:
+	case CFI_MFR_PMC: /* The ISSI/PMC is25lp064a use the same register/setting */
 		status = macronix_quad_enable(nor);
 		if (status) {
 			dev_err(nor->dev, "Macronix quad-read not enabled\n");
@@ -1195,6 +1197,8 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 			nor->read_opcode = SPINOR_OP_READ_1_1_4_D;
 		} else if (JEDEC_MFR(info) == CFI_MFR_MACRONIX) {
 			nor->read_opcode = SPINOR_OP_READ_1_4_4_D;
+		} else if (JEDEC_MFR(info) == CFI_MFR_PMC) { /* The ISSI/PMC is25lp064a */
+			nor->read_opcode = SPINOR_OP_READ_1_4_4_D;
 		} else {
 			dev_err(dev, "DDR Quad Read is not supported.\n");
 			return -EINVAL;
@@ -1254,6 +1258,14 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 	}
 
 	nor->read_dummy = spi_nor_read_dummy_cycles(nor);
+	if ((nor->flash_read == SPI_NOR_DDR_QUAD) &&
+	    (JEDEC_MFR(info) == CFI_MFR_PMC)) { /* The ISSI/PMC is25lp064a */
+		/* This is to handle the case where two different
+		   flash memories are used on the same hardware. The DTS
+		   can only have one "spi-nor,ddr-quad-read-dummy" and
+		   the two memories require different number of dummy cycles. */
+		nor->read_dummy = 2;
+	}
 
 	dev_info(dev, "%s (%lld Kbytes)\n", id->name,
 			(long long)mtd->size >> 10);
