b69ab778ef4dd742bb3cb205483a27b9691528e0
Andreas Rebert <andreas.rebert@embeddedartists.com>
2018-10-19 10:40:27 +0200

    PCI related (merge from 4.9.11)

--- drivers/pci/host/pci-imx6.c
+++ drivers/pci/host/pci-imx6.c
@@ -1275,7 +1275,7 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 
 		/* pcie phy ref clock select; 1? internal pll : external osc */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				BIT(5), 0);
+				BIT(5), BIT(5));
 	} else if (imx_pcie->variant == IMX6SX) {
 		/* Force PCIe PHY reset */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR5,
@@ -1446,7 +1446,7 @@ static void pci_imx_clk_disable(struct device *dev)
 	case IMX7D:
 		/* turn off external osc input */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IOMUXC_GPR12,
-				BIT(5), BIT(5));
+				BIT(5), 0);
 		break;
 	/*
 	 * Disable the over ride.
